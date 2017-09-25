41fc1db4363514d91f3b9fa8f43d691e0a490f20
Virginia Riego del Castillo <virginiardelc@gmail.com>
2017-09-25 14:13:24 +0200

    Add files via upload

---
+++ Introduccion/funciones.py
@@ -0,0 +1,8 @@
+def funcion():
+    print("Ha entrado en la ejecución de la funcion")
+
+def funcion_parametros(nombre):
+    print(nombre+ ", ha entrado en la ejecución de la funcion")
+
+funcion()
+funcion_parametros("Juan")
